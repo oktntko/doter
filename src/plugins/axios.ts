626ca4428e3f06826f59bcb7872c86fdbf5e9672
oktntko <oktntko@gmail.com>
2022-11-17 23:31:43 +0900

    textualize

--- src/plugins/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const client = axios.create();
-
-client.interceptors.request.use((config) => {
-  config.socketPath = process.env.SOCKET_PATH || "/var/run/docker.sock";
-  config.baseURL = `http:/${process.env.API_VERSION || "v1.41"}`;
-  return config;
-});
